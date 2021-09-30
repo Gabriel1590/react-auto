dc8ef59dc29555b6150ff4176c711fd4e31db828
gabriel <gabrielescorche@gmail.com>
2021-09-30 19:25:11 -0400

    Fixed linter generate.js

--- commands/generate.js
+++ commands/generate.js
@@ -15,11 +15,11 @@ async function init() {
 
 async function getTypeOfSchema() {
   let [typeOfSchema] = Array.from(process.argv).slice(3);
-    
+
   if (typeOfSchema === 'c' || typeOfSchema === 'component') {
-    typeOfSchema = 'component'
+    typeOfSchema = 'component';
   } else if (typeOfSchema === 'rc' || typeOfSchema === 'redux-component') {
-    typeOfSchema = 'redux-component'
+    typeOfSchema = 'redux-component';
   } else {
     const TEMPLATE_CHOICES = fs.readdirSync(`${__dirname}/../templates/schemas`);
 
@@ -28,15 +28,15 @@ async function getTypeOfSchema() {
         name: 'schema-choice',
         type: 'list',
         message: 'What schema template would you like to generate?',
-        choices: TEMPLATE_CHOICES
-      }
+        choices: TEMPLATE_CHOICES,
+      },
     ];
-    
+
     const answers = await inquirer.prompt(QUESTIONS);
     typeOfSchema = answers['schema-choice'];
   }
 
-  return typeOfSchema
+  return typeOfSchema;
 }
 
 async function getFileData() {
@@ -47,14 +47,14 @@ async function getFileData() {
         name: 'schema-location',
         type: 'input',
         message: 'Schema location:',
-        validate: function (input) {
+        validate(input) {
           if (/^([A-Za-z\-\_\d\/])+$/.test(input)) return true;
-          else return 'Schema location may only include letters, numbers, underscores and hashes.';
-        }
-      }
+          return 'Schema location may only include letters, numbers, underscores and hashes.';
+        },
+      },
     ];
     const answers = await inquirer.prompt(QUESTIONS);
-    
+
     location = answers['schema-location'];
   }
 
@@ -63,11 +63,11 @@ async function getFileData() {
   let name = splitedLocation.pop();
   name = name.charAt(0).toUpperCase() + name.slice(1);
 
-  if (!/^([A-Za-z\-\_\d])+$/.test(name) ) {
-    throw new Error('Schema name may only include letters, numbers, underscores and hashes.')
+  if (!/^([A-Za-z\-\_\d])+$/.test(name)) {
+    throw new Error('Schema name may only include letters, numbers, underscores and hashes.');
   }
 
-  return { location: splitedLocation.join('/') || '.', name }
+  return { location: splitedLocation.join('/') || '.', name };
 }
 
 function createSchema(typeOfSchema, { location, name }) {
@@ -76,38 +76,40 @@ function createSchema(typeOfSchema, { location, name }) {
 
   if (!dirExists) {
     fs.mkdirSync(writeDirPath);
-  } else {
-    if (fs.readdirSync(writeDirPath)[0]) {
-      throw new Error(`Error: Directory ${name} is not empty`)
-    }
+  } else if (fs.readdirSync(writeDirPath)[0]) {
+    throw new Error(`Error: Directory ${name} is not empty`);
   }
 
-  const templatePath = __dirname + `/../templates/schemas/${typeOfSchema}/ts`;
-  const filesToCreate = fs.readdirSync(templatePath)
+  const templatePath = `${__dirname}/../templates/schemas/${typeOfSchema}/ts`;
+  const filesToCreate = fs.readdirSync(templatePath);
 
-  filesToCreate.forEach(file => {
+  filesToCreate.forEach((file) => {
     const origFilePath = `${templatePath}/${file}`;
     let contents = fs.readFileSync(origFilePath, 'utf8');
 
     const writePath = `${writeDirPath}/${file}`;
-    contents = addNameToContent(typeOfSchema, name, contents)
+    contents = addNameToContent(typeOfSchema, name, contents);
     fs.writeFileSync(writePath, contents, 'utf8');
-
   });
 }
 
 function addNameToContent(typeOfSchema, name, contents) {
-  switch(typeOfSchema) {
+  let modifiedContent = contents;
+  switch (typeOfSchema) {
     case 'component': {
-      contents = contents.replace(/Component/g, `${name}Component`)
+      modifiedContent = modifiedContent.replace(/Component/g, `${name}Component`);
+      break;
     }
     case 'redux-component': {
-      contents = contents.replace(/Action/g, `${name}Action`)  
-      contents = contents.replace(/Reducer/g, `${name}Reducer`)  
-      contents = contents.replace(/State/g, `${name}State`)  
+      modifiedContent = modifiedContent.replace(/Action/g, `${name}Action`);
+      modifiedContent = modifiedContent.replace(/Reducer/g, `${name}Reducer`);
+      modifiedContent = modifiedContent.replace(/State/g, `${name}State`);
+      break;
     }
+    default:
+      return modifiedContent;
   }
-  return contents;
+  return modifiedContent;
 }
 
 init();
