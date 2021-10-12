93a1242d9df8cb8dac5d5947b70ca272456f6cc7
gabriel <gabrielescorche@gmail.com>
2021-10-12 06:41:52 -0400

    Created utils lib

--- commands/generate/component.js
+++ commands/generate/component.js
@@ -1,10 +1,53 @@
 /**
- * 
- * @param {string} name 
- * @param {string} location 
+ * @param {string} name
+ * @param {string} location
  */
 function generate(name, location) {
   
 }
 
-module.exports = { generate }
+const fs = require('fs');
+const { createDir } = require('../../lib/utils');
+
+const CURR_DIR = process.cwd();
+
+function createSchema(typeOfSchema = 'component', { location = '.', name = '', ext = 'ts' }) {
+  createDir(`${CURR_DIR}/${location}`, name);
+
+  const templatePath = `${__dirname}/../../templates/schemas/${typeOfSchema}/${ext}`;
+  const filesToCreate = fs.readdirSync(templatePath);
+
+  filesToCreate.forEach((file) => {
+    const origFilePath = `${templatePath}/${file}`;
+    let contents = fs.readFileSync(origFilePath, 'utf8');
+
+    const writePath = `${writeDirPath}/${file}`;
+    contents = addNameToContent(typeOfSchema, name, contents);
+    fs.writeFileSync(writePath, contents, 'utf8');
+  });
+
+  if (typeOfSchema === 'redux-config' && ext === 'ts') {
+    createSchema(typeOfSchema, { location: `${location}/../`, name: 'hooks', ext: 'hooks' });
+  }
+}
+
+function addNameToContent(typeOfSchema, name, contents) {
+  let modifiedContent = contents;
+  switch (typeOfSchema) {
+    case 'component': {
+      modifiedContent = modifiedContent.replace(/Component/g, `${name}Component`);
+      break;
+    }
+    case 'redux-component': {
+      modifiedContent = modifiedContent.replace(/Action/g, `${name}Action`);
+      modifiedContent = modifiedContent.replace(/Reducer/g, `${name}Reducer`);
+      modifiedContent = modifiedContent.replace(/State/g, `${name}State`);
+      break;
+    }
+    default:
+      return modifiedContent;
+  }
+  return modifiedContent;
+}
+
+module.exports = { generate };
